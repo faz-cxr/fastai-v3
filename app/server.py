6e8ad00cb3571a58895bb33d4925c5c15e5cb4fa
faz-cxr <47987464+faz-cxr@users.noreply.github.com>
2019-03-12 01:27:04 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1RCkpGFgNAsDih2UqzcTG3s2_5vEBZtRh'
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
