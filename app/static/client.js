f7111debd25a10f5bb928f9d79af07e131f075df
faz-cxr <47987464+faz-cxr@users.noreply.github.com>
2019-03-20 04:59:00 +0530

    Update client.js

--- app/static/client.js
+++ app/static/client.js
@@ -26,7 +26,7 @@ function analyze() {
             var response = JSON.parse(e.target.responseText);
             el('result-label').innerHTML = `Result = ${response['result']}`;
         }
-        el('analyze-button').innerHTML = 'Analyze';
+        el('analyze-button').innerHTML = 'Predict';
     }
 
     var fileData = new FormData();
